f0cc16cbb89d0e1395541154c165c911c2eac77a
Navjot Sherry <navjotsherryy@gmail.com>
2022-03-10 04:00:32 +0530

    deploytry5

--- carbocommerce/settings.py
+++ carbocommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pz)tsm$sk47%jao(ng0z1kon^2h#ak4da+)@75^b&v&0a$%2!+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carbocommerce.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
