4e728871c0a76b79bb131163f650fe793e433ff1
Navjot Sherry <navjotsherryy@gmail.com>
2022-03-10 05:08:53 +0530

    Please enter the commit message for your changes. Lines starting

--- carbocommerce/settings.py
+++ carbocommerce/settings.py
@@ -132,8 +132,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
